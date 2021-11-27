d9f62672bc72e31aac22d8591a6d95e36eb1c892
telsta <95098522+telsta@users.noreply.github.com>
2021-11-27 18:28:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Telsta Group ! ( PVR's initiative )" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Coming Soon <code>pages/index.js</code>
         </p>
       </main>
 
